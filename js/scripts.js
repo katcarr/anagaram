a04778c218990aba47d4e77dbbed3c7d7d9fe918
Kathryn Carr and Lauren Marcus <lauren.e.marcus@gmail.com>
2015-02-10 14:08:42 -0800

    Check a one word list for anagram

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
-var anagram = function() {
+var anagram = function(word, list) {
+
+  var results = [];
+
+  var letterizeWord = word.toLowerCase().split("").sort();
+  var letterizedList = list[0].toLowerCase().split("").sort();
+  var same = false;
+
+  if (letterizeWord.join("") === letterizedList.join("")){
+    results.push(list[0]);
+  }
 
 
-  
+  return results;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
 describe("anagram", function() {
   it("accepts a 2 element array where the second element is already an anagram of the first element, and will return the second element", function() {
-    expect(anagram("sub", ["bus"])).to.equal(["bus"]);
+    expect(anagram("sub", ["bus"])).to.eql(["bus"]);
   });
 });
